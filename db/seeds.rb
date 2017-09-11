2fcb42b2b6131f9c8b48667213eeda8a33ec9ecd
İbrahim S. YILMAZ <info@ibrahimserhat.com>
2017-09-11 11:29:18 +0300

    Added default datas to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+1..5.each do |i|
+  Blog.create(title: "Example Blog Title #{i}",
+              body: "Example body lorem ipsum #{i}")
+end
+
+1..5.each do |i|
+  Portfolio.create(title: "Example Portfolio Title #{i}",
+                   subtitle: "Example subtitle #{i}",
+                   body: "Example body lorem ipsum #{i}",
+                   main_image: '',
+                   thumb_image: '')
+end
+
+
+Skill.create([
+                 { title: 'Rails', percent_utilized: 85 },
+                 { title: 'HTML', percent_utilized: 90 },
+                 { title: 'Angular', percent_utilized: 10 }
+             ])
