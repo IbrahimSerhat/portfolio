2eae8cb0f6b9d727493f4500b4488a7e4ae19fd4
Ibrahim S. YILMAZ <info@ibrahimserhat.com>
2017-09-14 13:45:49 +0300

    Integrated database relationship between topics and blog posts

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,4 +5,6 @@ class Blog < ApplicationRecord
   enum status: { draft: 0, published: 1}
 
   validates_presence_of :title, :body
+
+  belongs_to :topic
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914091933) do
+ActiveRecord::Schema.define(version: 20170914104238) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -19,7 +19,9 @@ ActiveRecord::Schema.define(version: 20170914091933) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.integer "status", default: 0
+    t.integer "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -47,6 +49,12 @@ ActiveRecord::Schema.define(version: 20170914091933) do
   create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "percent_utilized"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
