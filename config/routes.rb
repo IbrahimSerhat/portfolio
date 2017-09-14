eae451276f2d274c1032c88ef7a5b3a5135734ac
Ibrahim S. YILMAZ <info@ibrahimserhat.com>
2017-09-14 12:37:49 +0300

    Implemented custom action for updating statues for blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
 
   resources :portfolios
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   root to: 'pages#home'
 end
