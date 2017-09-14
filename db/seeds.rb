0bc9b010007159448679a2df47c9da1ef918cca9
Ibrahim S. YILMAZ <info@ibrahimserhat.com>
2017-09-14 14:15:47 +0300

    Added custom content to seed file for example and development testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+3.times do |topic|
+  Topic.create!(
+           title: "Topic #{topic}"
+  )
+end
 
-(1..5).each do |i|
+(1..10).each do |i|
   Blog.create(title: "Example Blog Title #{i}",
-              body: "Example body lorem ipsum #{i}")
+              body: "Example body lorem ipsum #{i}",
+              topic_id: Random.rand(1..3)
+  )
 end
 
 (1..5).each do |i|
   Portfolio.create(title: "Example Portfolio Title #{i}",
-                   subtitle: "Example subtitle #{i}",
+                   subtitle: 'Ruby on Rails',
                    body: "Example body lorem ipsum #{i}",
+                   main_image: 'http://placehold.it/600x400',
+                   thumb_image: 'http://placehold.it/350x200')
+end
+
+(1..2).each do |i|
+  Portfolio.create(title: "Example Portfolio Title #{i+5}",
+                   subtitle: 'Angular',
+                   body: "Example body lorem ipsum #{i+5}",
                    main_image: 'http://placehold.it/600x400',
                    thumb_image: 'http://placehold.it/350x200')
 end
