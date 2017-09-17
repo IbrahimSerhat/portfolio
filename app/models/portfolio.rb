6d03728b8cb022ad2b7e1fa25ddd105fb8cdb375
Ibrahim S. YILMAZ <info@ibrahimserhat.com>
2017-09-17 15:54:12 +0300

    Integrated nested attributes for portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,7 +2,8 @@ class Portfolio < ApplicationRecord
   has_many :technologies
   includes Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
-
+  accepts_nested_attributes_for :technologies,
+                                reject_if: lambda { |attrs| attrs.blank? }
   after_initialize :set_defaults
 
   def set_defaults
