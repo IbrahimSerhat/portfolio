6d9e2b67f932c32f06c4b501fef9f87ce5a2cba2
Ibrahim S. YILMAZ <info@ibrahimserhat.com>
2017-09-17 11:43:47 +0300

    Implemented image default for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,10 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= 'http://placehold.it/600x400'
+    self.thumb_image ||= 'http://placehold.it/350x200'
+  end
 end
