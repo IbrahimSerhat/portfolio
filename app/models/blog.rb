a90794bae5c14c260b9cba98da1bab1e80df99e1
Ibrahim S. YILMAZ <info@ibrahimserhat.com>
2017-10-20 00:15:12 +0300

    Fixing dependent attributes

---
+++ app/models/blog.rb
@@ -0,0 +1,12 @@
+class Blog < ApplicationRecord
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  enum status: { draft: 0, published: 1}
+
+  validates_presence_of :title, :body
+
+  belongs_to :topic
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+end
