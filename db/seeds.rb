a7d0b1f74dc3b2ce3ab1d5ed61b805f6f672cbe9
İbrahim S. YILMAZ <info@ibrahimserhat.com>
2017-11-05 15:26:50 +0300

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,33 +10,32 @@ end
 (1..10).each do |i|
   Blog.create(title: "Example Blog Title #{i}",
               body: "Example body lorem ipsum #{i}",
-              topic_id: Random.rand(1..3), user_id: User.first.id
+              topic_id: Random.rand(1..3), user_id: User.first.id,
+              status: Random.rand(0..1)
   )
 end
 
-#(1..5).each do |i|
-#  Portfolio.create(title: "Example Portfolio Title #{i}",
-#                   subtitle: 'Ruby on Rails',
-#                   body: "Example body lorem ipsum #{i}",
-#                   main_image: 'uploads/main_image/600x400.png',
-#                   thumb_image: 'uploads/thumb_image/350x200.png')
-#  Portfolio.last.technologies.create(name: 'Rails')
-#end
+(1..5).each do |i|
+  Portfolio.create(title: "Example Portfolio Title #{i}",
+                   subtitle: 'Ruby on Rails',
+                   body: "Example body lorem ipsum #{i}"
+  )
+  Portfolio.last.technologies.create(name: 'Rails')
+end
 
-#(1..2).each do |i|
-#  Portfolio.create(title: "Example Portfolio Title #{i+5}",
-#                   subtitle: 'Angular',
-#                   body: "Example body lorem ipsum #{i+5}",
-#                   main_image: 'http://placehold.it/600x400',
-#                   thumb_image: 'http://placehold.it/350x200')
-#  Portfolio.last.technologies.create(name: 'Angular')
-#end
+(1..2).each do |i|
+  Portfolio.create(title: "Example Portfolio Title #{i+5}",
+                   subtitle: 'Angular',
+                   body: "Example body lorem ipsum #{i+5}"
+  )
+  Portfolio.last.technologies.create(name: 'Angular')
+end
 
 
 Skill.create([
-                 { title: 'Rails', percent_utilized: 85 },
+                 { title: 'Ruby on Rails', percent_utilized: 85 },
                  { title: 'HTML', percent_utilized: 90 },
-                 { title: 'Angular', percent_utilized: 10 }
+                 { title: 'Angular', percent_utilized: 25 }
              ])
 
 puts "demo admin login => email: #{demoadmin.email}, password: #{demoadmin.password}"
